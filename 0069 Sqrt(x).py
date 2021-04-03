90597a5d2ee9f39370bad54fa7b7fcfec7ffab74
jaredliw <jaredliw@gmail.com>
2021-04-03 12:53:08 +0800

    Q69

---
+++ 0069 Sqrt(x).py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # Runtime: 28 ms
+        # Memory: 14.2 MB
+        return int(sqrt(x))  # you may use isqrt for python >= 3.8
