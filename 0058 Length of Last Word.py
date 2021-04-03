19bc2d2c5a8dc475c424d6846bb1eb97d63465c0
jaredliw <jaredliw@gmail.com>
2021-04-03 13:31:16 +0800

    Q58 -- 2 solutions

---
+++ 0058 Length of Last Word.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Runtime: 12 ms
+        # Memory: 13.6 MB
+        words = s.split()
+        if not words:
+            return 0
+        else:
+            return len(words[-1])
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Runtime: 20 ms
+        # Memory: 13.4 MB
+        try:
+            return len(s.split()[-1])
+        except:
+            return 0
