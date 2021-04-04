b88f4e3a110d0a8ec8c0647536990a7374375f3c
jaredliw <jaredliw@gmail.com>
2021-04-04 22:21:12 +0800

    Q70 -- 2 solutions

---
+++ 0070 Climbing Stairs.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Runtime: 20 ms
+        # Memory: 13.3 MB
+
+        # It is actually a fibonacci sequence, where Xn = Xn-1 + Xn-2
+        # if we can climb 1, 2 or 3 steps at a time, then Xn = Xn-1 + Xn-2 + Xn-3 and so as for n steps
+        fib1 = 1
+        fib2 = 2
+        if n == 1:
+            return fib1
+        elif n == 2:
+            return fib2
+        for _ in range(n - 2):
+            fib1, fib2 = fib2, fib1 + fib2
+        return fib2
+
+
+from math import sqrt
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Runtime: 16 ms
+        # Memory: 13.4 MB
+
+        # Find nth fibonacci number by formula
+        # n + 1 because the sequence is start with "1, 2, ..." whereas the formula is for the sequence which starts
+        # with "1, 1, ..."
+        return int(((((1 + sqrt(5)) / 2) ** (n + 1)) - (((1 - sqrt(5)) / 2) ** (n + 1))) / sqrt(5))
