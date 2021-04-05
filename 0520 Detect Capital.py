702456691760f15b8a81a5cae75b0b37de8106d6
jaredliw <jaredliw@gmail.com>
2021-04-05 11:33:45 +0800

    Q520

---
+++ 0520 Detect Capital.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        # Runtime: 20 ms
+        # Memory: 13.6 MB
+        return word == word.upper() or word == word.lower() or (word[0] == word[0].upper() and word[1:] == word[1:].lower())
