30131775889ec35ece129f49c9651932e30f681c
jaredliw <jaredliw@gmail.com>
2021-04-05 11:02:46 +0800

    Q217

---
+++ 0217 Contains Duplicate.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Runtime: 92 ms
+        # Memory: 20 MB
+        return len(nums) != len(set(nums))
+
+
+from collections import Counter
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Runtime: 104 ms
+        # Memory: 19.6 MB
+        return Counter(nums).most_common(1)[0][1] != 1
