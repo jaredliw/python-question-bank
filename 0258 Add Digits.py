c0e2dfc96a8330c5f8fd40f14809341ed8d4bed7
jaredliw <jaredliw@gmail.com>
2021-04-05 15:15:37 +0800

    * Q258 -- 3 solutions

--- 0258 Add Digits.py
+++ 0258 Add Digits.py
@@ -19,6 +19,27 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
+        # Runtime: 16 ms
+        # Memory: 13.4 MB
+
+        # Direct formula:
+        #            r 0                        , if n = 0
+        # dr_b(n) = <
+        #            L 1 + ((n - 1) mod (b - 1)), if n != 0
+        return 0 if num == 0 else (1 + (num - 1) % 9)
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
         # Runtime: 20 ms
         # Memory: 13.4 MB
+
+        # Direct formula:
+        #            r 0            , if n = 0
+        # dr_b(n) = <  b - 1        , if n != 0 and n congruent 0 (mod (b - 1))
+        #            L n mod (b - 1), if n not congruent 0 (mod (b - 1))
         return 9 if num != 0 and num % 9 == 0 else num % 9
