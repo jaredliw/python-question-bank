e37422bb4f735d8ba2aacfc37839e9e13a677aaf
jaredliw <jaredliw@gmail.com>
2021-04-06 18:06:25 +0800

    Q383

---
+++ 0383 Ransom Note.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        # Runtime: 44 ms
+        # Memory: 13.9 MB
+        magazine = list(magazine)
+        try:
+            [magazine.remove(char) for char in ransomNote]
+            return True
+        except:
+            return False
+
