aec261d5fe8319c973982759b5aac6ec68b9b78d
jaredliw <jaredliw@gmail.com>
2021-04-07 20:12:01 +0800

    Q1598

---
+++ 1598 Crawler Log Folder.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        # Runtime: 32 ms
+        # Memory: 13.5 MB
+        return reduce(lambda depth, log: max(depth - 1, 0) if log == "../" else depth + 1 if log != "./" else depth,
+                      logs, 0)
