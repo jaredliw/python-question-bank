4cb041e8ed9c94f8321096fe48120f1260f7fa80
jaredliw <jaredliw@gmail.com>
2021-04-07 19:50:05 +0800

    Q1603

---
+++ 1603 Design Parking System.py
@@ -0,0 +1,25 @@
+class ParkingSystem(object):
+    # Runtime: 132 ms
+    # Memory: 13.9 MB
+    def __init__(self, big, medium, small):
+        """
+        :type big: int
+        :type medium: int
+        :type small: int
+        """
+        self.slots = [big, medium, small]
+
+    def addCar(self, carType):
+        """
+        :type carType: int
+        :rtype: bool
+        """
+        if self.slots[carType - 1] > 0:
+            self.slots[carType - 1] -= 1
+            return True
+        else:
+            return False
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
