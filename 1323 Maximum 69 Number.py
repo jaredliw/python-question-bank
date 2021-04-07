379c56decf7d11df0dbd57767b5dd8a2e81e6566
jaredliw <jaredliw@gmail.com>
2021-04-07 21:18:14 +0800

    Q1323

---
+++ 1323 Maximum 69 Number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximum69Number(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        # Runtime: 16 ms
+        # Memory: 13.4 MB
+        return int(str(num).replace("6", "9", 1))
