eaaf0474be70ef18e531170df8224d735d92aabd
jaredliw <jaredliw@gmail.com>
2021-04-12 16:51:51 +0800

    Q704

---
+++ 0704 Binary Search.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # Runtime: 188 ms
+        # Memory: 14.5 MB
+
+        # Binary search
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # Runtime: 196 ms
+        # Memory: 14.7 MB
+
+        # Linear search
+        try:
+            return nums.index(target)
+        except:
+            return -1
