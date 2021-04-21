909292944f2e86e81fa7161f1930d8534812ca00
jaredliw <jaredliw@gmail.com>
2021-04-21 15:52:21 +0800

    Q912

---
+++ 0912 Sort an Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Runtime: 112 ms
+        # Memory: 20.2 MB
+
+        # For more sorting algorithms, see:
+        # https://github.com/jaredliw/PyDSA/blob/master/pydsa/algorithms/sorting.py
+        return sorted(nums)
