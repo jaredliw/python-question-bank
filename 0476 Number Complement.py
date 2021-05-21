c0dc2e2ced33566bd9e8aa63c8450fb630437f0d
jaredliw <jaredliw@gmail.com>
2021-05-21 20:02:51 +0800

    Q476 -- 3 solutions

--- 0476 Number Complement.py
+++ 0476 Number Complement.py
@@ -24,3 +24,14 @@ class Solution(object):
 
         # n ^ complement = 111... <-> n ^ 111... = complement
         return num ^ (2 ** int(log(num, 2) + 1) - 1)  # Find how many 1 is needed
+
+
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        # Runtime: 24 ms
+        # Memory: 13.3 MB
+        return num ^ (2 ** num.bit_length() - 1)
