6a941d9a849e63f4562068eca7143042f5eedf8e
jaredliw <jaredliw@gmail.com>
2021-05-23 08:40:50 +0800

    Q1207

---
+++ 1207 Unique Number of Occurrences.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        # Runtime: 20 ms
+        # Memory: 13.6 MB
+        counter = {}
+        for num in arr:
+            counter.setdefault(num, 0)
+            counter[num] += 1
+        
+        return len(set(counter.values())) == len(counter.values())
