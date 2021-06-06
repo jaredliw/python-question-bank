4421c888660e0f4f0563b5dce3d701cdcbc80e6e
jaredliw <jaredliw@gmail.com>
2021-06-06 15:28:34 +0800

    Reformat

--- kattis/Quality-Adjusted Life-Year.py
+++ kattis/Quality-Adjusted Life-Year.py
@@ -2,12 +2,10 @@
 # from sys import stdin
 # from functools import reduce
 #
-#
 # input()
 # print(reduce(lambda x, line: x + float(line.split()[0]) * float(line.split()[1]), stdin.readlines(), 0))
 
 # CPU: 0.02 s
 from sys import stdin
-
 
 print(eval(''.join(stdin.readlines()[1:]).replace('\n', '+').replace(' ', '*')[:-1]))
