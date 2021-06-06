f6ab61ab01f64e76fbf8c5f3ad22502f76dc32fd
jaredliw <jaredliw@gmail.com>
2021-06-06 13:30:26 +0800

    quadrant

---
+++ kattis/Quadrant Selection.py
@@ -0,0 +1,9 @@
+x_positive, y_positive = int(input()) > 0, int(input()) > 0
+if x_positive and y_positive:
+    print(1)
+elif x_positive:
+    print(4)
+elif y_positive:
+    print(2)
+else:
+    print(3)
