50e9cba2948b38f19b913eaa42e9aea9dee572d9
jaredliw <jaredliw@gmail.com>
2021-06-27 08:47:24 +0800

    sok

---
+++ kattis/Sok.py
@@ -0,0 +1,7 @@
+# CPU: 0.05 s
+amounts = list(map(int, input().split()))
+ratios = list(map(int, input().split()))
+
+n = min(amounts[x] / ratios[x] for x in range(3))
+for amount, ratio in zip(amounts, ratios):
+	print(amount - ratio * n, end=" ")
