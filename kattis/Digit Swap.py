5db3adc818d56ae1f2aea0260f9a035c7783f604
jaredliw <jaredliw@gmail.com>
2021-12-02 23:47:02 +0800

    digitswap

---
+++ kattis/Digit Swap.py
@@ -0,0 +1,12 @@
+# CPU: 0.05 s
+print(input()[::-1])
+
+# CPU: 0.05 s
+# Pure math solution
+# num = int(input())
+# new_num = 0
+# while num != 0:
+# 	new_num *= 10
+# 	new_num += num % 10
+# 	num //= 10
+# print(new_num)
