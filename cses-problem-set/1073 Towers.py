56140b4aabe8fb2390e1995547946cbf6caa524c
jaredliw <jaredliw@gmail.com>
2021-12-18 00:21:46 +0800

    add comment

--- cses-problem-set/1073 Towers.py
+++ cses-problem-set/1073 Towers.py
@@ -1,6 +1,8 @@
 # Time: 0.12 s
 input()
-towers = []  # Store the topmost cube of each tower
+# Store the topmost cube of each tower
+# Towers are in non-decreasing order for sure
+towers = []
 for cube in map(int, input().split()):
 	# Binary search
 	left = 0
@@ -14,8 +16,12 @@ for cube in map(int, input().split()):
 			right = mid - 1
 			tower_idx = mid
 
+	# If there aren't any suitable towers, append new tower to end of
+	# array
 	if tower_idx != -1:
 		towers[tower_idx] = cube
+	# If there exists a satisfying tower, add the cube to that tower
+	# and update the top element of the tower
 	else:
 		towers.append(cube)
 print(len(towers))
