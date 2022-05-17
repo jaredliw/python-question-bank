e2a17c444c15a8c259af26ab1b717aea471f417d
wolfishLamb <jaredliw@gmail.com>
2022-05-17 21:31:46 +0800

    * 69

--- leetcode/0069 Sqrt(x).py
+++ leetcode/0069 Sqrt(x).py
@@ -1,12 +1,31 @@
-from math import sqrt
-
-
 class Solution(object):
     def mySqrt(self, x):
         """
         :type x: int
         :rtype: int
         """
-        # Runtime: 28 ms
-        # Memory: 14.2 MB
-        return int(sqrt(x))  # you may use isqrt for python >= 3.8
+        # Runtime: 37 ms
+        # Memory: 13.3 MB
+        left = 0
+        right = x
+        while left < right:
+            mid = (left + right + 1) // 2
+            if mid ** 2 > x:
+                right = mid - 1
+            else:
+                left = mid
+        return left
+
+
+# from math import sqrt
+#
+#
+# class Solution(object):
+#     def mySqrt(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+#         # Runtime: 28 ms
+#         # Memory: 14.2 MB
+#         return int(sqrt(x))  # you may use isqrt for python >= 3.8
